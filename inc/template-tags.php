372cf9b4a51b77369992bb0bd64cf21a4b88c4e8
I.Smal <serbov@pochta.ru>
2016-09-13 00:57:59 +0300

    [3] Скрыл для постов из category дату, автора и комментарий

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -18,42 +18,44 @@ if ( ! function_exists( 'twentysixteen_entry_meta' ) ) :
  * @since Twenty Sixteen 1.0
  */
 function twentysixteen_entry_meta() {
-	if ( 'post' === get_post_type() ) {
-		$author_avatar_size = apply_filters( 'twentysixteen_author_avatar_size', 49 );
-		printf( '<span class="byline"><span class="author vcard">%1$s<span class="screen-reader-text">%2$s </span> <a class="url fn n" href="%3$s">%4$s</a></span></span>',
-			get_avatar( get_the_author_meta( 'user_email' ), $author_avatar_size ),
-			_x( 'Author', 'Used before post author name.', 'twentysixteen' ),
-			esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-			get_the_author()
-		);
-	}
+	if(! has_category( 'catalog' )) {
+		if ( 'post' === get_post_type() ) {
+			$author_avatar_size = apply_filters( 'twentysixteen_author_avatar_size', 49 );
+			printf( '<span class="byline"><span class="author vcard">%1$s<span class="screen-reader-text">%2$s </span> <a class="url fn n" href="%3$s">%4$s</a></span></span>',
+				get_avatar( get_the_author_meta( 'user_email' ), $author_avatar_size ),
+				_x( 'Author', 'Used before post author name.', 'twentysixteen' ),
+				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+				get_the_author()
+			);
+		}
 
-	if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
-		twentysixteen_entry_date();
-	}
+		if ( in_array( get_post_type(), array( 'post', 'attachment' ) ) ) {
+			twentysixteen_entry_date();
+		}
 
-	$format = get_post_format();
-	if ( current_theme_supports( 'post-formats', $format ) ) {
-		printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
-			sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'twentysixteen' ) ),
-			esc_url( get_post_format_link( $format ) ),
-			get_post_format_string( $format )
-		);
-	}
+		$format = get_post_format();
+		if ( current_theme_supports( 'post-formats', $format ) ) {
+			printf( '<span class="entry-format">%1$s<a href="%2$s">%3$s</a></span>',
+				sprintf( '<span class="screen-reader-text">%s </span>', _x( 'Format', 'Used before post format.', 'twentysixteen' ) ),
+				esc_url( get_post_format_link( $format ) ),
+				get_post_format_string( $format )
+			);
+		}
 
-	if ( 'post' === get_post_type() ) {
-		twentysixteen_entry_taxonomies();
-	}
+		if ( 'post' === get_post_type() ) {
+			twentysixteen_entry_taxonomies();
+		}
 
-	if ( ! is_singular() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'twentysixteen' ), get_the_title() ) );
-		echo '</span>';
+		if ( ! is_singular() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo '<span class="comments-link">';
+			comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'twentysixteen' ), get_the_title() ) );
+			echo '</span>';
+		}
 	}
 }
 endif;
 
-if ( ! function_exists( 'twentysixteen_entry_date' ) ) :
+if ( ! function_exists( 'twentysixteen_entry_date' )) :
 /**
  * Prints HTML with date information for current post.
  *
@@ -62,24 +64,30 @@ if ( ! function_exists( 'twentysixteen_entry_date' ) ) :
  * @since Twenty Sixteen 1.0
  */
 function twentysixteen_entry_date() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if(! has_category( 'catalog' )) {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( 'c' ) ),
+			get_the_date(),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			get_the_modified_date()
+		);
+
+		printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
+			_x( 'Posted on', 'Used before publish date.', 'twentysixteen' ),
+			esc_url( get_permalink() ),
+			$time_string
+		);
+	} else {
+		return null;
 	}
 
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		get_the_date(),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		get_the_modified_date()
-	);
 
-	printf( '<span class="posted-on"><span class="screen-reader-text">%1$s </span><a href="%2$s" rel="bookmark">%3$s</a></span>',
-		_x( 'Posted on', 'Used before publish date.', 'twentysixteen' ),
-		esc_url( get_permalink() ),
-		$time_string
-	);
 }
 endif;
 
@@ -92,20 +100,22 @@ if ( ! function_exists( 'twentysixteen_entry_taxonomies' ) ) :
  * @since Twenty Sixteen 1.0
  */
 function twentysixteen_entry_taxonomies() {
-	$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentysixteen' ) );
-	if ( $categories_list && twentysixteen_categorized_blog() ) {
-		printf( '<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
-			_x( 'Categories', 'Used before category names.', 'twentysixteen' ),
-			$categories_list
-		);
-	}
+	if(! has_category( 'catalog' )) {
+		$categories_list = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentysixteen' ) );
+		if ( $categories_list && twentysixteen_categorized_blog() ) {
+			printf( '<span class="cat-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
+				_x( 'Categories', 'Used before category names.', 'twentysixteen' ),
+				$categories_list
+			);
+		}
 
-	$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'twentysixteen' ) );
-	if ( $tags_list ) {
-		printf( '<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
-			_x( 'Tags', 'Used before tag names.', 'twentysixteen' ),
-			$tags_list
-		);
+		$tags_list = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'twentysixteen' ) );
+		if ( $tags_list ) {
+			printf( '<span class="tags-links"><span class="screen-reader-text">%1$s </span>%2$s</span>',
+				_x( 'Tags', 'Used before tag names.', 'twentysixteen' ),
+				$tags_list
+			);
+		}
 	}
 }
 endif;
